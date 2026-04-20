7918cc537465a446ce844f8949dcf5650067e405
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2026-04-20 17:35:32 +0200

    fix: Provide ways to find a browser in flake
    xdg-utils now provided, user environment's PATH is appended, BROWSER
    envvar is set to firefox if not present (default fallback)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Japanese popup dictionary as a native window";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     fenix = {
       url = "github:nix-community/fenix";
@@ -10,15 +10,17 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    crane,
-    fenix,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      crane,
+      fenix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -27,10 +29,12 @@
         fenix-pkgs = fenix.packages.${system};
         fenix-channel = fenix-pkgs.stable;
         fenix-toolchain = fenix-channel.toolchain;
-        craneLib = (crane.mkLib pkgs).overrideScope (final: prev: {
-          cargo = fenix-channel.cargo;
-          rustc = fenix-channel.rustc;
-        });
+        craneLib = (crane.mkLib pkgs).overrideScope (
+          final: prev: {
+            cargo = fenix-channel.cargo;
+            rustc = fenix-channel.rustc;
+          }
+        );
         runtimeInputs = with pkgs; [
           wayland
           libxkbcommon
@@ -46,69 +50,83 @@
         ];
         dependencyPrograms = with pkgs; [
           tesseract
+          xdg-utils
         ];
-        buildtimeInputs = with pkgs; [
+        buildtimeInputs =
+          with pkgs;
+          [
             fenix-pkgs.rust-analyzer
             fenix-toolchain
-	    pkg-config
+            pkg-config
             onnxruntime
-        ] ++ runtimeInputs;
+          ]
+          ++ runtimeInputs;
       in
       {
         packages =
-        let
-          unfilteredRoot = ./.;
-          src = lib.fileset.toSource {
-            root = unfilteredRoot;
-            fileset = lib.fileset.unions [
-              # Default files from crane (Rust and cargo files)
-              (craneLib.fileset.commonCargoSources unfilteredRoot)
-              # Folder for images and fonts
-              (lib.fileset.maybeMissing ./src/assets)
-            ];
-          };
-        in rec {
-          unwrapped = with pkgs; craneLib.buildPackage {
-            nativeBuildInputs = buildtimeInputs;
-	    buildInputs = buildtimeInputs;
+          let
+            unfilteredRoot = ./.;
+            src = lib.fileset.toSource {
+              root = unfilteredRoot;
+              fileset = lib.fileset.unions [
+                # Default files from crane (Rust and cargo files)
+                (craneLib.fileset.commonCargoSources unfilteredRoot)
+                # Folder for images and fonts
+                (lib.fileset.maybeMissing ./src/assets)
+              ];
+            };
+          in
+          rec {
+            unwrapped =
+              with pkgs;
+              craneLib.buildPackage {
+                nativeBuildInputs = buildtimeInputs;
+                buildInputs = buildtimeInputs;
 
-            # Needed for the unit tests.
+                # Needed for the unit tests.
+                LD_LIBRARY_PATH = lib.makeLibraryPath runtimeInputs;
+
+                cargoExtraArgs = "--all-features";
+
+                inherit src;
+                strictDeps = true;
+              };
+
+            # ORT_DYLIB_PATH is necessary for Onnx Runtime. It puts the responsibility of providing the shared library on Nix instead of on ORT's downloader.
+            # https://blog.stark.pub/posts/bundling-onnxruntime-rust-nix/
+            default =
+              pkgs.runCommandLocal "popup_dictionary"
+                {
+                  nativeBuildInputs = [
+                    pkgs.makeWrapper
+                  ];
+                }
+                ''
+                  mkdir -p $out/bin
+                  ln -s ${unwrapped}/bin/popup_dictionary $out/bin
+                  wrapProgram $out/bin/popup_dictionary \
+                    --set LD_LIBRARY_PATH ${lib.makeLibraryPath runtimeInputs} \
+                    --prefix PATH : ${lib.makeBinPath dependencyPrograms} \
+                    --set ORT_DYLIB_PATH ${pkgs.onnxruntime}/lib/libonnxruntime.so \
+                    --set-default BROWSER firefox
+                '';
+          };
+
+        devShells.default =
+          with pkgs;
+          pkgs.mkShell {
+            buildInputs = [
+              bashInteractive
+            ]
+            ++ buildtimeInputs
+            ++ dependencyPrograms;
+
             LD_LIBRARY_PATH = lib.makeLibraryPath runtimeInputs;
-            
-            cargoExtraArgs = "--all-features";
 
-            inherit src;
-            strictDeps = true;
+            shellHook = ''
+              export SHELL=${pkgs.bashInteractive}/bin/bash
+            '';
           };
-
-          # ORT_DYLIB_PATH is necessary for Onnx Runtime. It puts the responsibility of providing the shared library on Nix instead of on ORT's downloader.
-          # https://blog.stark.pub/posts/bundling-onnxruntime-rust-nix/
-          default = pkgs.runCommandLocal "popup_dictionary" {
-            nativeBuildInputs = [
-              pkgs.makeWrapper
-            ];
-          }
-          ''
-            mkdir -p $out/bin
-            ln -s ${unwrapped}/bin/popup_dictionary $out/bin
-            wrapProgram $out/bin/popup_dictionary \
-              --set LD_LIBRARY_PATH ${lib.makeLibraryPath runtimeInputs} \
-              --set PATH ${lib.makeBinPath dependencyPrograms} \
-              --set ORT_DYLIB_PATH ${pkgs.onnxruntime}/lib/libonnxruntime.so
-          '';
-        };
-
-        devShells.default = with pkgs; pkgs.mkShell {
-          buildInputs = [
-	    bashInteractive
-          ] ++ buildtimeInputs ++ dependencyPrograms;
-
-          LD_LIBRARY_PATH = lib.makeLibraryPath runtimeInputs;
-
-	  shellHook = ''
-            export SHELL=${pkgs.bashInteractive}/bin/bash
-          '';
-        };
 
       }
     );
