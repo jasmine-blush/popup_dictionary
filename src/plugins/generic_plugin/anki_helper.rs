49dcdc8b0d70f2fced7d290b7df47a6752244e97
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2026-08-23 17:06:17 +0200

    feat: Add more JPMN fields and improve definition

--- src/plugins/generic_plugin/anki_helper.rs
+++ src/plugins/generic_plugin/anki_helper.rs
@@ -6,7 +6,7 @@ use tracing::{debug, warn};
 
 use crate::{
     plugin::Token,
-    plugins::generic_plugin::generic_helper::{GenericDefinition, GenericWord},
+    plugins::generic_plugin::generic_helper::{GenericCategory, GenericDefinition, GenericWord},
 };
 
 pub fn add_note(
@@ -17,16 +17,116 @@ pub fn add_note(
     let mut sentence = sentence.replace(surface, &format!("<b>{}</b>", surface));
 
     let word = definition.get_word();
-    let meanings = definition
-        .get_meanings()
-        .iter()
-        .map(|m| m.get_glosses().join(", "))
-        .collect::<Vec<String>>()
-        .join("<br>");
+
+    let mut meanings = String::new();
+    let mut counter: usize = 0;
+    let mut prev_categories: String = String::new();
+    for meaning in definition.get_meanings() {
+        // Display tags only if they aren't the same as previous meanings
+        let categories: String = meaning.get_categories().join("");
+        if categories != prev_categories {
+            prev_categories = categories.to_owned();
+
+            let mut tags: String = String::from("<div>");
+            if !meanings.is_empty() {
+                tags.insert_str(0, "</ol></div>");
+            }
+            for category in meaning.get_categories() {
+                tags.push_str(&format!("
+<span data-sc-code=\"{}\" style=\"font-weight: bold; font-size: 0.8em; color: white; background-color: rgb(86, 86, 86); vertical-align: text-bottom; border-radius: 0.3em; margin-right: 0.25em; padding: 0.2em 0.3em; word-break: keep-all; cursor: help;\" title=\"{}\">{}</span>"
+                    , category, GenericCategory::get_info(category), category));
+            }
+            meanings.push_str(&format!("{}<ol>", tags));
+        }
+        if counter == 0 {
+            counter = 1;
+        }
+
+        let mut list_for_multiple_glosses = String::new();
+        if meaning.get_glosses().len() < 2 {
+            list_for_multiple_glosses
+                .push_str("style=\"padding-left: 0px; list-style-type: none;\"");
+        }
+        let circled_counter = char::from_u32(0x2460 + (counter - 1) as u32).unwrap_or('\u{2460}');
+        let mut glosses: String = String::from(&format!(
+            "
+<li style=\"padding-left: 0.25em; list-style-type: &quot;{}&quot;;\">
+    <ul data-sc-content=\"glossary\" {}>",
+            circled_counter, list_for_multiple_glosses
+        ));
+        for gloss in meaning.get_glosses() {
+            glosses.push_str(&format!("<li>{}</li>", gloss));
+        }
+        glosses.push_str("</ul>");
+
+        if let Some(infos) = meaning.get_infos() {
+            if !infos.is_empty() {
+                glosses.push_str(&format!(
+                    "
+<div data-sc-content=\"extra-info\" style=\"margin-left: 0.5em;\">
+    <div data-sc-content=\"sense-note\" style=\"background-color: color-mix(in srgb, goldenrod 5%, transparent); border-color: goldenrod; border-style: none none none solid; border-radius: 0.4rem; border-width: calc(3em / var(--font-size-no-units, 14)); margin-top: 0.5rem; margin-bottom: 0.5rem; padding: 0.5rem;\">
+        <div style=\"font-style: italic; font-size: 0.8em; color: rgb(119, 119, 119);\">Note</div>
+        <div style=\"margin-left: 0.5rem;\">{}</div>
+    </div>
+</div>", infos.join("; ")
+                ));
+            }
+        }
+
+        glosses.push_str("</li>");
+
+        meanings.push_str(&glosses);
+
+        counter += 1;
+    }
+    if !meanings.is_empty() {
+        meanings.push_str("</ol></div>");
+    }
+
+    let primary_definition = format!(
+        "
+<ol>
+    <li data-details=\"{}\">
+        <span class=\"dict-group__tag-list\">
+            <span class=\"dict-group__tag dict-group__tag--name\">
+                <span class=\"dict-group__tag-inner\">★</span>
+            </span>
+            <span class=\"dict-group__tag dict-group__tag--dict\">
+                <span class=\"dict-group__tag-inner\">{}</span>
+            </span>
+        </span>
+        <span class=\"dict-group__glossary\">
+            <span>
+                {}
+            </span>
+        </span>
+    </li>
+</ol>
+",
+        "Kihon", "Kihon", meanings
+    );
+
     let frequencies = if let Some(frequencies) = definition.get_frequencies() {
         let str = frequencies
             .iter()
-            .map(|f| format!("{}<br>{}<br>", f.rank, f.source))
+            .map(|f| {
+                format!(
+                    "
+<div class=\"frequencies__group\" data-details=\"{}\">
+    <div class=\"frequencies__number\">
+        <span class=\"frequencies__number-inner\">
+            {}
+        </span>
+    </div>
+    <div class=\"frequencies__dictionary\">
+        <span class=\"frequencies__dictionary-inner\">
+            {}
+        </span>
+    </div>
+</div>",
+                    f.source, f.rank, f.source
+                )
+            })
             .collect::<Vec<String>>()
             .join("");
 
@@ -54,10 +154,13 @@ pub fn add_note(
                     "Key": word.get_surface(),
                     "Word": word.get_surface(),
                     "WordReading": get_word_furigana_string(word),
-                    "PrimaryDefinition": meanings,
+                    "PAOverride": "-1",
+                    "PrimaryDefinition": primary_definition,
                     "Sentence": sentence,
                     "FrequenciesStylized": frequencies,
-                    "FrequencySort": frequency_sort
+                    "FrequencySort": frequency_sort,
+                    "PASilence": "[sound:_silence.wav]",
+                    "WordReadingHiragana": word.get_kana(),
                 },
                 "options": {
                     "allowDuplicate": true,
@@ -97,7 +200,10 @@ fn get_word_furigana_string(word: &GenericWord) -> String {
                 if !furigana_string.is_empty() && !furigana_string.ends_with("]") {
                     furigana_string.push_str(" ");
                 }
-                furigana_string.push_str(&format!("{}[{}]", furigana.base, furigana.reading));
+                furigana_string.push_str(&format!("{}", furigana.base));
+                if furigana.base != furigana.reading {
+                    furigana_string.push_str(&format!("[{}]", furigana.reading));
+                }
             }
             return furigana_string;
         }
